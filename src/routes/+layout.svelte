97cfd815292a5cc92de8702beab617f6aee2ecbb
benjamin schreiber <bpschreiber2003@gmail.com>
2023-06-11 15:27:15 -0700

    further initialization, added logo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<!-- 
+    layout is that applies itself to everything within the same
+    directory. This is where you can add global styles, scripts, etc.
+
+    <slot /> is where the content of the page will be injected
+-->
+<script>
+    import "../styles/tailwind.css";
+</script>
+
+<style global>
+    :global(body) {
+        background-color: #282c34;
+    }
+</style>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img 
+    height=500 
+    class="flex justify-center max-h-full m-5" 
+    src="src/lib/assets/logo.svg" 
+    alt="Logo" 
+/>
