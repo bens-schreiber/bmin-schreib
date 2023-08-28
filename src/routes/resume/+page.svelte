776da192f2c59dc5a39ef5cd39b86b0e59b65a89
benjamin schreiber <bpschreiber2003@gmail.com>
2023-08-27 20:13:52 -0700

    Added dividers, changed some wording/ordering in the resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import SVGTile from './components/SVGTile.svelte';
 	import LogoIconifyTile from './components/LogoIconifyTile.svelte';
-	import CornerIcon from '../home/components/CornerIcon/CornerIcon.svelte';
 </script>
 
+<!-- WORK EXPERIENCE-->
 <h1 class="font-black text-white text-5xl text-center mt-5">BENJAMIN SCHREIBER</h1>
+<div class="border-t border-gray-300 my-8 mx-2" />
 
 <SVGTile
 	svg="src/lib/assets/IntelliTect_WhiteBlue-Box.svg"
@@ -15,10 +16,12 @@
 <SVGTile
 	svg="src/lib/assets/washington-state-cougars.svg"
 	heading="Washington State University"
-	subheading="Computer Science - 2026"
+	subheading="Computer Science - Graduating 2026"
 />
 
+<!-- PROGRAMMING LANGUAGES -->
 <h1 class="font-black text-white text-5xl text-center mt-12">LANGUAGES</h1>
+<div class="border-t border-gray-300 my-8 mx-2" />
 
 <div class="grid grid-cols-2 gap-4">
 	<LogoIconifyTile
@@ -46,7 +49,10 @@
 	</span>
 </div>
 
+<!-- CLOUD RELATED PROGRAMMING SKILLS -->
 <h1 class="font-black text-white text-5xl text-center mt-12">CLOUD SKILLS</h1>
+<div class="border-t border-gray-300 my-8 mx-2" />
+
 <div class="grid grid-cols-2 gap-4">
 	<LogoIconifyTile heading="Azure" subheading="Web Apps, BLOB Storage" icon="devicon:azure" />
 
@@ -69,22 +75,18 @@
 	/>
 
 	<span class="col-span-2"
-		><LogoIconifyTile heading="Swagger" subheading="API Documentation" icon="logos:swagger" /></span
+		><LogoIconifyTile heading="Swagger" subheading="API Documentation, OAuth2" icon="logos:swagger" /></span
 	>
 </div>
 
+<!-- ACHIEVEMENTS, AWARDS -->
 <h1 class="font-black text-white text-5xl text-center mt-12">ACCOLADES</h1>
-
-<SVGTile
-	svg="src/lib/assets/IntelliTect_WhiteBlue-Box.svg"
-	heading="IntelliTect Scholar"
-	subheading="2022 Overall Winner"
-/>
+<div class="border-t border-gray-300 my-8 mx-2" />
 
 <SVGTile
 	svg="src/lib/assets/COA.png"
 	heading="Triangle STEM Fraternity"
-	subheading="Vice President and Chapter Founder"
+	subheading="2023 Vice President and Chapter Founder"
 	customHeight={30}
 />
 
@@ -109,3 +111,8 @@
 	customHeight={30}
 />
 
+<SVGTile
+	svg="src/lib/assets/IntelliTect_WhiteBlue-Box.svg"
+	heading="IntelliTect Scholar"
+	subheading="2022 Overall Winner"
+/>
