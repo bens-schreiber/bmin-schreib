956dd637d601b43af6979325f9de6a5ae78d5d55
benjamin schreiber <bpschreiber2003@gmail.com>
2023-10-01 16:48:23 -0700

    style changes

--- src/routes/components/NavBar.svelte
+++ src/routes/components/NavBar.svelte
@@ -1,30 +1,30 @@
 <script>
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
 </script>
 
-<nav class="flex justify-around" style="max-width: 800px;">
-    <a
-        href="home"
-        class="w-1/3 {$page.url.pathname === '/home'
-            ? 'bg-bg-primary'
-            : 'bg-bg-tertiary'} px-8 py-3 border-r border-r-slate-900 text-center text-white font-bold text-sm"
-    >
-        CONTACT
-    </a>
-    <a
-        href="resume"
-        class="w-1/3 {$page.url.pathname === '/resume'
-            ? 'bg-bg-primary'
-            : 'bg-bg-tertiary'} px-8 py-3 border-r border-r-slate-900 text-center text-white font-bold text-sm"
-    >
-        RESUME
-    </a>
-    <a
-        href="portfolio"
-        class="w-1/3 {$page.url.pathname === '/portfolio'
-            ? 'bg-bg-primary'
-            : 'bg-bg-tertiary'} px-8 py-3 text-center text-white font-bold text-sm"
-    >
-        WORK
-    </a>
+<nav class="flex" style="max-width: 800px;">
+	<a
+		href="home"
+		class="w-1/3 {$page.url.pathname === '/home'
+			? 'bg-bg-primary'
+			: 'bg-bg-tertiary'} px-8 py-3 border-r border-r-slate-900 text-center text-white font-medium text-sm"
+	>
+		CONTACT
+	</a>
+	<a
+		href="resume"
+		class="w-1/3 {$page.url.pathname === '/resume'
+			? 'bg-bg-primary'
+			: 'bg-bg-tertiary'} px-8 py-3 border-r border-r-slate-900 text-center text-white font-medium text-sm"
+	>
+		RESUME
+	</a>
+	<a
+		href="portfolio"
+		class="w-1/3 {$page.url.pathname === '/portfolio'
+			? 'bg-bg-primary'
+			: 'bg-bg-tertiary'} px-8 py-3 text-center text-white font-medium text-sm"
+	>
+		WORK
+	</a>
 </nav>
