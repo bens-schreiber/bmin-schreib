3894a09d7cbcd87feb4428ef29751bca35e8e355
Ben Schreiber <bpschreiber2003@gmail.com>
2023-12-08 13:27:20 -0800

    Added another raylib project, Vector Fields

--- src/routes/portfolio/items/RayLib.svelte
+++ src/routes/portfolio/items/RayLib.svelte
@@ -3,7 +3,8 @@
     import Icon from '@iconify/svelte';
     import PortfolioProject from '../components/PortfolioProject.svelte';
 	const raylibLogo = new URL('/src/lib/assets/raylib-logo.png', import.meta.url).href;
-	const raylibDemo = new URL('/src/lib/assets/newton-phys-demo.mov', import.meta.url).href;
+	const raylibNewtonPhysics = new URL('/src/lib/assets/newton-phys-demo.mov', import.meta.url).href;
+	const raylibVectorFields = new URL('/src/lib/assets/vector-field-demo.mov', import.meta.url).href;
 </script>
 
 <PortfolioProject
@@ -21,16 +22,37 @@
 			<h1 class="text-4xl text-white font-bold">About</h1>
 			<div class="border-t border-gray-300 my-3" />
 			<p class="text-white text-xl leading-loose">
-				Raylib is a wrapper for OpenGL. I created a few projects to help me understand some concepts
-				I was learning in Calculus 3, Linear Algebra and Physics. I would make a new project for
-				each physics unit, such as newtonian laws and motion, creating an entire world from scratch
-				to simulate the physics concepts. Most of these projects were made in a day or two, and
-				employed the concepts I was learning in Calculus 3 and Linear Algebra to make the 3D world.
+				RayLib is a wrapper for OpenGL that allows fast development of 2D and 3D games and simulations.
+				Throughout my time at university, I utilized Raylib to help understand concepts in 
+				Calculus, Multivariable Calculus, Differential Equations, Physics and Linear Algebra.
+				These projects were developed in a variety of languages, including C, C++ and Golang.
 			</p>
 		</Tile>
 
 		<Tile>
-			<h1 class="text-4xl text-white font-bold">Newton Physics Example</h1>
+			<h1 class="text-4xl text-white font-bold">Multivariable Vector Fields</h1>
+			<div class="border-t border-gray-300 my-3" />
+			<a
+				href="https://github.com/bens-schreiber/raylib-vector-fields"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="flex items-center justify-center space-x-2 mt-8 text-white underline"
+			>
+				<Icon icon="akar-icons:github-fill" class="text-2xl text-white" />
+				<span class="text-white">View on Github</span>
+			</a>
+			<video
+				class="w-full mt-5"
+				controls
+				poster={raylibLogo}
+				src={raylibVectorFields}
+			>
+				<track kind="captions" src="path/to/captions.vtt" label="English" />
+			</video>
+		</Tile>
+
+		<Tile>
+			<h1 class="text-4xl text-white font-bold">Newton Physics</h1>
 			<div class="border-t border-gray-300 my-3" />
 			<a
 				href="https://github.com/bens-schreiber/newtons-laws-raylib/tree/main"
@@ -45,7 +67,7 @@
 				class="w-full mt-5"
 				controls
 				poster={raylibLogo}
-				src={raylibDemo}
+				src={raylibNewtonPhysics}
 			>
 				<track kind="captions" src="path/to/captions.vtt" label="English" />
 			</video>
