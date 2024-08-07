65f3c468e5bdbc93661148d5034979249d870ef9
Ben Schreiber <bpschreiber2003@gmail.com>
2024-08-07 00:19:25 -0700

    fix load + pushapp page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,34 +4,29 @@
 	import { onMount } from 'svelte';
 	import NavBar from './components/NavBar.svelte';
 	import Contact from './contact/components/Contact.svelte';
+	$: innerWidth = 0;
+	$: loading = true;
 
 	onMount(() => {
 		document.title = 'Benjamin Schreiber';
+		setTimeout(() => {
+			// if the innerWidth is greater than 768px, route off of the /contact page if on it
+			if (innerWidth > 768 && $page.url.pathname === '/contact') {
+				window.location.href = '/resume';
+			}
+			loading = false;
+		}, 250); // the people like the load
 	});
-
-	// Tailwind media queries are being strange in this case, so I am resorting
-	// to binding the innerWidth and innerHeight of the window
-	$: innerWidth = 0;
-	$: innerHeight = 0;
-
-	// if the innerWidth is greater than 768px, route off of the /contact page if on it
-	$: if (innerWidth > 768 && $page.url.pathname === '/contact') {
-		window.location.href = '/resume';
-	}
 </script>
 
-<svelte:window bind:innerWidth bind:innerHeight />
+<svelte:window bind:innerWidth />
 
 <!--
 	For the desktop version, the navbar/contact component is used on every page, so
 	we a grid with a fixed NavBar and Contact.
-
-	We are going to initially query to see if innerHeight and innerWidth
-	are set to 0, and if they are, render nothing. This functons as an await,
-	we don't want to render anything until we are sure that we are rendering
-	the right thing.
 -->
-{#if innerHeight == 0 && innerWidth == 0}
+
+{#if loading || $page.url.pathname == '/pushapp'}
 	<div />
 {:else if innerWidth > 768}
 	<div class="flex">
