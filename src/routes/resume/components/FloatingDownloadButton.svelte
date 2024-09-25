79c05335a55cd491026efbf56423ba130103ea2d
Ben Schreiber <bpschreiber2003@gmail.com>
2024-09-25 14:18:35 -0700

    button not clickable

--- src/routes/resume/components/FloatingDownloadButton.svelte
+++ src/routes/resume/components/FloatingDownloadButton.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  import Icon from '@iconify/svelte';
+	import Icon from '@iconify/svelte';
 
-  const pdfUrl = new URL('/src/lib/assets/BenjaminSchreiberResume.pdf', import.meta.url).href;
+	const pdfUrl = new URL('/src/lib/assets/BenjaminSchreiberResume.pdf', import.meta.url).href;
 
-  // Function to download a pdf from lib/assets
-    const downloadPDF = () => {
-        const link = document.createElement('a');
-        link.href = pdfUrl;
-        link.download = 'BenjaminSchreiberResume.pdf';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+	// Function to download a pdf from lib/assets
+	const downloadPDF = () => {
+		const link = document.createElement('a');
+		link.href = pdfUrl;
+		link.download = 'BenjaminSchreiberResume.pdf';
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
 </script>
 
 <!-- Circular button -->
-<div class="fixed bottom-4 right-4">
+<div class="fixed bottom-4 right-4 z-10">
 	<button
-        on:click={downloadPDF}
+		on:click={downloadPDF}
 		class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-md"
 	>
-    <Icon class="h-8 w-8" icon="mdi:download" />
+		<Icon class="h-8 w-8" icon="mdi:download" />
 	</button>
 </div>
